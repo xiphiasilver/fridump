b3cfbd19f70829af3611e7c8e7c507fc06fcbfd1
xiphiasilver <33470910+xiphiasilver@users.noreply.github.com>
2018-12-07 11:09:44 +0900

    Update fridump.py
    add a functionality that connect remote host over tcp

--- fridump.py
+++ fridump.py
@@ -33,6 +33,8 @@ def MENU():
                         help='provide full output directory path. (def: \'dump\')')
     parser.add_argument('-U', '--usb', action='store_true',
                         help='device connected over usb')
+    parser.add_argument('-H', '--host', type=str, metavar="ip",
+                        help='device connected over tcp')        
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='verbose')
     parser.add_argument('-r', '--read-only', action='store_true',
@@ -53,6 +55,7 @@ arguments = MENU()
 APP_NAME = arguments.process
 DIRECTORY = ""
 USB = arguments.usb
+HOST = arguments.host
 DEBUG_LEVEL = logging.INFO
 STRINGS = arguments.strings
 MAX_SIZE = 20971520
@@ -71,6 +74,9 @@ session = None
 try:
     if USB:
         session = frida.get_usb_device().attach(APP_NAME)
+    elif HOST is not None:
+        device = frida.get_device_manager().add_remote_device(HOST)
+        session = device.attach(int(APP_NAME))        
     else:
         session = frida.attach(APP_NAME)
 except Exception as e:
